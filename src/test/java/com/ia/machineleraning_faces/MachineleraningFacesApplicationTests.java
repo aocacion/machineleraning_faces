7413b99be0ba9228d48c0041e4c60cc0f7d55155
Alexis Ocaciones Garcia <alexis.ocacionesg@perficient.com>
2022-07-28 21:24:00 -0500

    Initial commit

---
+++ src/test/java/com/ia/machineleraning_faces/MachineleraningFacesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.machineleraning_faces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineleraningFacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
