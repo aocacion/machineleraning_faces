d6c7eee97aa90ef09991edd6de18a46a61a28851
Alexis Ocaciones Garcia <alexis.ocacionesg@perficient.com>
2022-12-11 17:24:17 -0500

    Inicio rostro

---
+++ server/rostros/src/main/java/com/ia/rostros/RostrosApplication.java
@@ -0,0 +1,13 @@
+package com.ia.rostros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RostrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RostrosApplication.class, args);
+	}
+
+}
---
+++ server/rostros/src/test/java/com/ia/rostros/RostrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ia.rostros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RostrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
